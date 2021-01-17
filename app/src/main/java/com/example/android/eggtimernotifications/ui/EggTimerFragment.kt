57529a6a6241bfae2eed74b176d1ea20f0364ec4
Antony <vontonnie@gmail.com>
2021-01-17 15:10:36 +0300

    Create Notification Channel

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -18,32 +18,29 @@ package com.example.android.eggtimernotifications.ui
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.eggtimernotifications.R
 import com.example.android.eggtimernotifications.databinding.FragmentEggTimerBinding
-import com.google.firebase.messaging.FirebaseMessaging
 
 class EggTimerFragment : Fragment() {
 
     private val TOPIC = "breakfast"
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         val binding: FragmentEggTimerBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_egg_timer, container, false
-        )
+                inflater, R.layout.fragment_egg_timer, container, false)
 
         val viewModel = ViewModelProvider(this).get(EggTimerViewModel::class.java)
 
@@ -51,12 +48,42 @@ class EggTimerFragment : Fragment() {
         binding.lifecycleOwner = this.viewLifecycleOwner
 
         // TODO: Step 1.7 call create channel
+        createChannel(getString(R.string.egg_notification_channel_id), getString(R.string.egg_notification_channel_name))
 
         return binding.root
     }
 
     private fun createChannel(channelId: String, channelName: String) {
         // TODO: Step 1.6 START create a channel
+        //channels available from API26 - so check API level here
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            //create channel
+            val notificationChannel = NotificationChannel(
+                    channelId, channelName,
+
+                    //TODO Step 2.4 change importance
+                    NotificationManager.IMPORTANCE_LOW)
+            //TODO: Step 2.6 disable badges for this channel
+
+
+            //customize the notificationChannel
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Time for breakfast"
+
+            //get instance of NotificationManager
+
+
+            val notificationManager = requireActivity().getSystemService(NotificationManager::class.java)
+
+
+            //create channel off NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+
 
         // TODO: Step 1.6 END create a channel
 
